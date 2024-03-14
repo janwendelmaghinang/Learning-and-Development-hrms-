382ffef71cdc1c61d42be98b7500202eb8144b5c
Maghinang <janwendelmaghinang@gmail.com>
2024-03-14 20:45:40 +0800

    update ui

--- application/views/designations/index.php
+++ application/views/designations/index.php
@@ -6,7 +6,7 @@
   <section class="content-header">
     <h1>
       Manage
-      <small>Designations</small>
+      <small>Designation</small>
     </h1>
     <ol class="breadcrumb">
       <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
